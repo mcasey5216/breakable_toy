20c62598c2f2e4d414c50aaea0580d0ab0837cdd
Michelle Casey <mcasey5216>
2015-10-06 16:17:35 -0400

    hounded

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,8 @@
 class Contact < ActiveRecord::Base
   belongs_to :group
 
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, format:
+    { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
