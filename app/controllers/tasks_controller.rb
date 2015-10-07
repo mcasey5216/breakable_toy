7f54340e19d9d0677078aaa2e5c0f730396bbc23
Michelle Casey <mcasey5216>
2015-10-07 17:48:34 -0400

    hounded

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,23 +7,4 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @group = @task.group
   end
-
-  def create
-    @users = User.all
-    @gtask = Group.find(params[:id])
-    @membership = Membership.new(membership_params)
-    if @membership.save
-      flash[:success] = "Updates Successful"
-      redirect_to group_path(@group)
-    else
-      flash[:alert] = "Nope"
-      render :index
-    end
-  end
-
-  protected
-
-  def membership_params
-    params.require(:membership).permit(:user_id).merge(group: @task.group, task: @task)
-  end
 end
