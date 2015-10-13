763b8fb24a62effcb8793917fcb7f9037773a5c8
Michelle Casey <mcasey5216>
2015-10-13 15:11:02 -0400

    side nav on group show edited for styling, accordion function added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,11 +19,10 @@
 //= require autocomplete-rails
 //= require underscore
 //= require gmaps/google
+//= require vendor/modernizr
 //= require_tree .
 
-$(function(){
-  $(document).foundation('tab', 'reflow');
-
+  $(function(){ $(document).foundation(); });
   // checkin field set on new page
   $('.checkin-contact select, .checkin-contact input').attr('disabled', 'true');
   $('.checkin-contact').addClass('disable-field-set');
@@ -59,10 +58,73 @@ $(function(){
     nameDiv.innerHTML = popUpData.first_name + ' ' + popUpData.last_name;
     var emailDiv = document.createElement('div');
     emailDiv.innerHTML = popUpData.email;
+    var phoneDiv = document.createElement('div');
+    phoneDiv.innerHTML = popUpData.phone;
     wrapperDiv.appendChild(nameDiv);
     wrapperDiv.appendChild(emailDiv);
+    wrapperDiv.appendChild(phoneDiv);
     /* This is the different base on json object for all */
 
+
+    /* This is the same for all */
+    $('#pop-up-content').html(wrapperDiv);
+    /* This is the same for all */
+  });
+
+  // group show page side bar
+  $('#side-task li').hover(function(){
+    /* This is the same for all */
+    $('.pop-up').removeClass('hide');
+    var popUpOffSet = $(this).offset();
+    popUpOffSet.left += $(this).width();
+    $('.pop-up').offset(popUpOffSet);
+    var popUpData = JSON.parse($(this).attr('meta-data'));
+    /* This is the same for all */
+
+    /* This is the different base on json object for all */
+    var wrapperDiv = document.createElement('div');
+    var titleDiv = document.createElement('div');
+    titleDiv.innerHTML = popUpData.title;
+    var descriptionDiv = document.createElement('div');
+    descriptionDiv.innerHTML = popUpData.description;
+    var completedDiv = document.createElement('div');
+    completedDiv.innerHTML = 'completed? ' + popUpData.completed;
+    wrapperDiv.appendChild(titleDiv);
+    wrapperDiv.appendChild(descriptionDiv);
+    wrapperDiv.appendChild(completedDiv);
+    /* This is the different base on json object for all */
+
+
+    /* This is the same for all */
+    $('#pop-up-content').html(wrapperDiv);
+    /* This is the same for all */
+  });
+
+  // group show page side bar
+  $('#side-contact li').hover(function(){
+    /* This is the same for all */
+    $('.pop-up').removeClass('hide');
+    var popUpOffSet = $(this).offset();
+    popUpOffSet.left += $(this).width();
+    $('.pop-up').offset(popUpOffSet);
+    var popUpData = JSON.parse($(this).attr('meta-data'));
+    /* This is the same for all */
+
+    /* This is the different base on json object for all */
+    var wrapperDiv = document.createElement('div');
+    var companyDiv = document.createElement('div');
+    companyDiv.innerHTML = popUpData.company_name;
+    var nameDiv = document.createElement('div');
+    nameDiv.innerHTML = popUpData.first_name + ' ' + popUpData.last_name;
+    var phoneDiv = document.createElement('div');
+    phoneDiv.innerHTML = popUpData.phone;
+    var whereDiv = document.createElement('div');
+    whereDiv.innerHTML = popUpData.city + ', ' + popUpData.state;
+    wrapperDiv.appendChild(companyDiv);
+    wrapperDiv.appendChild(nameDiv);
+    wrapperDiv.appendChild(phoneDiv);
+    wrapperDiv.appendChild(whereDiv);
+    /* This is the different base on json object for all */
 
     /* This is the same for all */
     $('#pop-up-content').html(wrapperDiv);
@@ -74,7 +136,6 @@ $(function(){
   });
 
   $('#search').keyup(function() {
-    //debugger;
     var query = $(this).val();
     var userList = $('#all-users li');
 
@@ -88,4 +149,3 @@ $(function(){
 
     }
   });
-});
